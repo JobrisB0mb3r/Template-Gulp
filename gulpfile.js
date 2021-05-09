8c7775bb076fff1aa97cdda4d90c8401aa338308
JobrisB0mb3r <81431775+JobrisB0mb3r@users.noreply.github.com>
2021-05-09 08:51:55 +0800

    Adding some styling build

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,29 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 // Utils initialize
 const rename = require('gulp-rename');
+
+// Defining some path so we can use that in our function
+var jalur = {
+    style: {
+        src: 'resources/scss/style.scss',
+        dest: 'public/css/'
+    }
+};
+
+function styling() {
+    return gulp.src(jalur.style.src)
+        .pipe(sass({ outputStyle: 'compressed', sourceMap: 'True' }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(jalur.style.dest));
+}
+
+function pengawas() {
+    gulp.watch('resources/scss/**/*.scss', styling);
+}
+
+var building = gulp.series(styling);
+
+exports.styling = styling;
+
+exports.default = building;
