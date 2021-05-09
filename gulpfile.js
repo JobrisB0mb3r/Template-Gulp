c4e534251ef33d48f4e5fa595e20346ac15f1fd2
JobrisB0mb3r <81431775+JobrisB0mb3r@users.noreply.github.com>
2021-05-09 15:02:05 +0800

    Adding build system for javascript stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+// Javascript stuff initialize
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 // Utils initialize
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
@@ -13,6 +17,10 @@ var jalur = {
     style: {
         src: 'resources/scss/style.scss',
         dest: 'public/css/'
+    },
+    scripts: {
+        src: 'resources/js/**/*.js',
+        dest: 'public/js/'
     }
 };
 
@@ -22,16 +30,30 @@ function styling() {
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('.', { includeContent: 'false', sourceRoot: '../resources/scss' }))
+        .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../resources/scss' }))
         .pipe(gulp.dest(jalur.style.dest));
 }
 
+function scripting() {
+    return gulp.src(jalur.scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(concat('main.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../resources/js' }))
+        .pipe(gulp.dest(jalur.scripts.dest));
+}
+
 function pengawas() {
+    gulp.watch(jalur.scripts.src, scripting);
     gulp.watch('resources/scss/**/*.scss', styling);
 }
 
-var building = gulp.series(styling);
+var building = gulp.parallel(styling, scripting);
 
 exports.styling = styling;
+exports.scripting = scripting;
+exports.pengawas = pengawas;
 
 exports.default = building;
