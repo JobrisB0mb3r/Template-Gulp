8d821bd6b7836d7afe0e1f2616508dd108a46a75
JobrisB0mb3r <81431775+JobrisB0mb3r@users.noreply.github.com>
2021-05-09 08:30:16 +0800

    Adding gulpfile and some basic stuff

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// Style initialize 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+// Utils initialize
+const rename = require('gulp-rename');
